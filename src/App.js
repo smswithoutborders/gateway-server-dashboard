b947e7d18064d1bda02dd4a13b1a2c1a518c94cf
Mildred <99267408+mildrette@users.noreply.github.com>
2024-03-31 00:58:06 +0100

    used material ui table to display data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from "./logo.svg";
 import "./App.css";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Dashboard from "./Pages/Dashboard";
+import ResponsiveDrawer from "./Components/Nav";
+import Chart from "./Pages/Charts";
+import Loader from "./Components/Loader";
+import DialogTable from "./Pages/TableDialog";
+import Footer from "./Components/Footer";
+import Toggle from "./Components/ThemeToggle";
 
 function App() {
+	const [isLoading, setIsLoading] = useState(true);
+	const [darkMode, setDarkMode] = useState(true);
+
+	useEffect(() => {
+		setTimeout(() => {
+			setIsLoading(false);
+		}, 3000);
+	}, []);
+
+	const toggleDarkMode = () => {
+		setDarkMode((prevMode) => !prevMode);
+	};
+
+	const theme = createTheme({
+		palette: {
+			mode: darkMode ? "dark" : "light"
+		}
+	});
+
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
+		<>
+			{isLoading ? (
+				<Loader />
+			) : (
+				<ThemeProvider theme={theme}>
+					<CssBaseline />
+					<Router>
+						<ResponsiveDrawer darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+						<Toggle darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+						<Routes>
+							{" "}
+							<Route path="/" element={<Dashboard />} />
+							<Route path="/charts" element={<Chart />} />
+							<Route path="/data" element={<DialogTable />} />
+						</Routes>
+						<Footer />
+					</Router>
+				</ThemeProvider>
+			)}
+		</>
 	);
 }
 
---
+++ src/Components/Table.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from "react";
+import MUIDataTable from "mui-datatables";
+import { Grid } from "@mui/material";
+
+const columns = [
+	"id",
+	"msisdn",
+	"country",
+	"operator",
+	"regdate",
+	"routed",
+	"success",
+	"failure",
+	"error"
+];
+
+const options = {
+	filterType: "checkbox"
+};
+
+function App() {
+	const [data, setData] = useState([]);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await fetch("https://mockapi.io/projects/660549352ca9478ea17ff01a");
+				const jsonData = await response.json();
+				// Map the fetched data to fit the columns
+				const mappedData = jsonData.map((item) => ({
+					msisdn: item.msisdn,
+					country: item.country,
+					operator: item.operator,
+					regdate: new Date(item.regdate).toLocaleDateString(),
+					routed: item.routed,
+					success: item.success,
+					failure: item.failure,
+					error: item.error,
+					id: item.id
+				}));
+				setData(mappedData);
+			} catch (error) {
+				console.error("Error fetching data:", error);
+			}
+		};
+
+		fetchData();
+	}, []);
+
+	return (
+		<div className="App bg-slate-700 py-10 min-h-screen grid place-items-center">
+			<div className="w-10/12 max-w-4xl">
+				<Grid container spacing={3}>
+					<Grid item md={12}>
+						<MUIDataTable title={"Employee List"} data={data} columns={columns} options={options} />
+					</Grid>
+				</Grid>
+			</div>
+		</div>
+	);
+}
+
+export default App;
